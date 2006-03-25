3ac6b2f153aebc9fb3e7e5229cad617a34c07a4f
pbienst <>
2006-03-25 15:57:49 +0000

    Fix rebuild queue after activate categories.

--- mnemosyne/mnemosyne/pyqt_ui/main_dlg.py
+++ mnemosyne/mnemosyne/pyqt_ui/main_dlg.py
@@ -446,6 +446,11 @@ class MainDlg(MainFrm):
         self.updateStatusBar()
         if not in_revision_queue(self.item):
             self.newQuestion()
+        else:
+            self.updateQuestion()
+            remove_from_revision_queue(self.item) # It's already being asked.
+        if number_of_items() == 0:
+            self.editItemsAction.setEnabled(0)
         unpause_thinking()
 
     ##########################################################################
