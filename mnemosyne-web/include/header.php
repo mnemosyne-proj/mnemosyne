04d0b43028f8d06969b8abb1639187a9771f7300
pbienst <>
2007-12-18 19:39:14 +0000

    Web update.

--- mnemosyne-web/include/header.php
+++ mnemosyne-web/include/header.php
@@ -18,13 +18,14 @@
 <img src="pictures/mnemosyne_logo.png">
 	
 <h3><a href="index.php">Overview</a></h3>
-<h3><a href="news.php">News</a></h3>
 <h3><a href="principles.php">Principles</a></h3>
+<h3><a href="features.php">Features</a></h3>
 <h3><a href="screenshots.php">Screenshots</a></h3>
 <h3><a href="http://sourceforge.net/project/showfiles.php?group_id=159201">Download</a></h3>
 <h3><a href="faq.php">Documentation</a></h3>
 <h3><a href="http://sourceforge.net/mail/?group_id=159201">Mailing lists</a></h3>
 <h3><a href="http://sourceforge.net/projects/mnemosyne-proj">Sourceforge page</a></h3>
+<h3><a href="news.php">News</a></h3>
 <h3><a href="history.php">History</a></h3>
 
 </div>
