6c714fec6c387a15a9427e1da0efb41532c94f4e
pbienst <>
2008-03-16 08:16:59 +0000

    Mark release date.

--- mnemosyne-web/include/header.php
+++ mnemosyne-web/include/header.php
@@ -4,6 +4,7 @@
 <html>
 
 <head>
+<meta http-equiv="REFRESH" content="0;url=http://www.mnemosyne-proj.org/index.php">
 <meta HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=iso-8859-1">
 <title>The Mnemosyne Project</title>
 <link rel="stylesheet" href="style.css" type="text/css" />
@@ -12,20 +13,3 @@
 </head>
 
 <body>
-
-<div id="menubar">
-
-<img src="pictures/mnemosyne_logo.png">
-	
-<h3><a href="index.php">Overview</a></h3>
-<h3><a href="principles.php">Principles</a></h3>
-<h3><a href="features.php">Features</a></h3>
-<h3><a href="screenshots.php">Screenshots</a></h3>
-<h3><a href="http://sourceforge.net/project/showfiles.php?group_id=159201">Download</a></h3>
-<h3><a href="faq.php">Documentation</a></h3>
-<h3><a href="community.php">Community</a></h3>
-<h3><a href="http://sourceforge.net/projects/mnemosyne-proj">Sourceforge page</a></h3>
-<h3><a href="news.php">Changelog</a></h3>
-<h3><a href="history.php">History</a></h3>
-
-</div>
