c2681f198996b8c7fdb2200dd8b13764b2a87756
pbienst <>
2008-07-22 14:39:17 +0000

    Update.

--- mnemosyne/mnemosyne/pyqt_ui/review_wdgt.py
+++ mnemosyne/mnemosyne/pyqt_ui/review_wdgt.py
@@ -70,3 +70,161 @@ answer
 </body>
 </html>
 """)
+
+
+        self.controller = controller
+
+        self.card = None # To controller?
+
+        self.grade_buttons = []
+
+        self.grade_buttons.append(self.grade_0_button)
+        self.grade_buttons.append(self.grade_1_button)
+        self.grade_buttons.append(self.grade_2_button)
+        self.grade_buttons.append(self.grade_3_button)
+        self.grade_buttons.append(self.grade_4_button)
+        self.grade_buttons.append(self.grade_5_button)
+
+        self.controller.new_question()
+        self.updateDialog()
+
+
+
+    ##########################################################################
+    #
+    # show_answer
+    #
+    ##########################################################################
+
+    def show_answer(self):
+
+        self.controller.show_answer() # TODO: update signal/slot
+
+
+
+    ##########################################################################
+    #
+    # gradeAnswer
+    #
+    ##########################################################################
+
+    def gradeAnswer(self, grade):
+
+        # TODO: optimise by displaying new question before grading the
+        # answer, provided the queue contains at least one card.
+
+        interval = self.controller.grade_answer()
+
+        self.newQuestion()
+        self.updateDialog()
+
+        if get_config("show_intervals") == "statusbar":
+            self.statusbar.message(self.trUtf8("Returns in ").append(\
+                str(interval)).append(self.trUtf8(" day(s).")))
+
+
+
+    ##########################################################################
+    #
+    # next_rep_string
+    #
+    ##########################################################################
+
+    def next_rep_string(self, days):
+
+        if days == 0:
+            return QString('\n') + self.trUtf8("Next repetition: today.")
+        elif days == 1:
+            return QString('\n') + self.trUtf8("Next repetition: tomorrow.")
+        else:
+            return QString('\n') + self.trUtf8("Next repetition in ").\
+                   append(QString(str(days))).\
+                   append(self.trUtf8(" days."))
+
+
+    def set_window_title(self, title):
+        self.setWindowTitle(title)
+
+    def enable_edit_current_card(self, enable):
+        self.actionEditCurrentCard.setEnabled(enable)
+
+    def enable_delete_current_card(self, enable):
+        self.actionDeleteCurrentCard.setEnabled(enable)
+
+    def enable_edit_deck(self, enable):
+        self.actionEditDeck.setEnabled(enable)
+
+    def get_font_size(self):
+        return font.pointSize()
+
+    def set_question_label(self, text):
+        self.question_label.setText(text)
+
+    def set_question(self, text):
+        self.question.setText(text)
+
+    def set_answer(self, text):
+        self.answer.setText(text)
+
+    def update_show_button(self, text, default, show_enabled):
+        self.show_button.setText(text)
+        self.show_button.setDefault(default)
+        self.show_button.setEnabled(show_enabled)
+
+
+    ##########################################################################
+    #
+    # update_dialog
+    #
+    #   Contains the updating of the dialog that is not specifically
+    #   handled by the UI controller.
+    #
+    ##########################################################################
+
+    def updateDialog(self):
+
+        # Update toolbar.
+
+        if get_config("hide_toolbar") == True:
+            self.toolBar.hide()
+            self.actionShowToolbar.setChecked(0)
+        else:
+            self.toolBar.show()
+            self.actionShowToolbar.setChecked(1)
+
+        # Update question and answer font.
+
+        if get_config("QA_font") != None:
+            font = QFont()
+            font.fromString(get_config("QA_font"))
+        else:
+            font = self.show_button.font()
+
+        self.question.setFont(font)
+        self.answer.setFont(font)
+
+        # Update question and answer alignment.
+
+        # TODO: reimplement
+
+        #if get_config("left_align") == True:
+        #    alignment = Qt.AlignAuto    | Qt.AlignVCenter | Qt.TextWordWrap
+        #else:
+        #    alignment = Qt.AlignHCenter | Qt.AlignVCenter | Qt.TextWordWrap
+
+        #self.question.setAlignment(alignment)
+        #self.answer.setAlignment(alignment)
+
+        # Update status bar.
+
+        self.sched .setText(self.trUtf8("Scheduled: ").append(QString(\
+                            str(scheduled_cards()))))
+        self.notmem.setText(self.trUtf8("Not memorised: ").append(QString(\
+                            str(non_memorised_cards()))))
+        self.all   .setText(self.trUtf8("All: ").append(QString(\
+                            str(active_cards()))))
+
+        # TODO: autoshrinking?
+        #if self.shrink == True:
+        #    self.adjustSize()
+
