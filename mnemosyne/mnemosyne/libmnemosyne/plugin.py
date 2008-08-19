029ef21b5b5b909abb38d07b8861e0cc97a88104
pbienst <>
2008-08-19 10:26:44 +0000

    Update plugin.

--- mnemosyne/mnemosyne/libmnemosyne/plugin.py
+++ mnemosyne/mnemosyne/libmnemosyne/plugin.py
@@ -1,62 +1,31 @@
-##############################################################################
 #
 # plugin.py <Peter.Bienstman@UGent.be>
 #
-##############################################################################
 
 from mnemosyne.libmnemosyne.component import Component
 from mnemosyne.libmnemosyne.component_manager import component_manager
 
 
-
-##############################################################################
-#
-# Plugin
-#
-#   A plugin is a component which can be activated and deactivated by the
-#   user when the program is running. In addition to the standard
-#   implementation of these (de)activation routines below, a plugin
-#   typically (un)register one or more other components (or 'self', if one
-#   uses multiple inheritance).
-#
-##############################################################################
-
 class Plugin(Component):
 
-    ##########################################################################
-    #
-    # __init__
-    #
-    ##########################################################################
-
-    def __init__(self, name, description):
-
-        self.name         = name
-        self.description  = description
-        self.active       = False
-
+    """A plugin is a component which can be activated and deactivated by the
+    user when the program is running. In addition to the standard
+    implementation of these (de)activation routines below, a plugin
+    typically (un)register one or more other components (or 'self', if one
+    uses multiple inheritance).
+    
+    """
+    
+    def __init__(self):
+        self.name = ""
+        self.description = ""
+        self.version = None
+        self.author = ""
+        self.active = False
         component_manager.register("plugin", self)
 
-
-
-    ##########################################################################
-    #
-    # activate
-    #
-    ##########################################################################
-
     def activate(self):
-
         self.active = True
 
-
-
-    ##########################################################################
-    #
-    # deactivate
-    #
-    ##########################################################################
-
     def deactivate(self):
-
         self.active = False
