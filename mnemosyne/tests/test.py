6bf7105fbcdb902e981e02a23a90e6e702feab15
pbienst <>
2009-03-01 09:28:38 +0000

    Start working on tests.

--- mnemosyne/tests/test.py
+++ mnemosyne/tests/test.py
@@ -1,42 +1,27 @@
-#TODO: clean up
+import os
 
-from mnemosyne.libmnemosyne.component_manager import component_manager as cm
-from mnemosyne.libmnemosyne.config import config as cfg
+from mnemosyne.libmnemosyne import initialise, finalise
+from mnemosyne.libmnemosyne.component_manager import ui_controller_main
+from mnemosyne.libmnemosyne.component_manager import card_type_by_id
 
-from mnemosyne.libmnemosyne.card_types.front_to_back import FrontToBack
-from mnemosyne.libmnemosyne.card_types.both_ways import BothWays
-from mnemosyne.libmnemosyne.card_types.three_sided import ThreeSided
-
-t1 = FrontToBack(); cm.register("card_type", t1)
-t2 = BothWays(); cm.register("card_type", t2)
-t3 = ThreeSided(); cm.register("card_type", t3)
+#def setup_package():
+#    print 'hi'
+#    initialise("/home/pbienst/source/mnemosyne-trunk/mnemosyne-proj/mnemosyne/dot_test")
 
 
-cfg.initialise("\dev\\null")
-cfg["path"] = "\dev\\null"
+#def teardown_package():
+#    finalise()
 
-from mnemosyne.libmnemosyne import *
-#initialise_system_components()
-#config.initialise(basedir)
-#initialise_lockfile()
-#initialise_new_empty_database()
 
-from mnemosyne.libmnemosyne.databases.pickle import Pickle
-
-cm.register("database", Pickle())
-
-from mnemosyne.libmnemosyne.schedulers.SM2_mnemosyne \
-                                                   import SM2Mnemosyne
-
-cm.register("scheduler", SM2Mnemosyne())
-
-db = cm.get_current("database")
-db.new("\dev\null")
 def test_add_cards():
+    os.system("rm -fr dot_test")
+    initialise("dot_test")
+    fact_data = {"q" : "question", "a" : "answer"}
+    card_type = card_type_by_id("1")
+    ui_controller_main().create_new_cards(fact_data, card_type,
+                                          grade=0, cat_names=["default"])
 
-    data = {"q" : "question", "a" : "answer"}
-    t1.create_new_cards(data, grade=0, cat_names=["default"])
+    ui_controller_main().file_save()
+    finalise()
 
-    db.save("\dev\\null")
-
-
+test_add_cards()
