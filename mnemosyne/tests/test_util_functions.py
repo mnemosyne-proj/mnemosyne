321a37123c8319940f7a42a373385df6f7282b8a
pbienst <>
2009-03-03 17:13:07 +0000

    Natural sorting order (Mike Appleby).

---
+++ mnemosyne/tests/test_util_functions.py
@@ -0,0 +1,28 @@
+from mnemosyne.libmnemosyne.utils import *
+
+class TestUtilFunctions:
+
+    def test_numeric_string_cmp_1(self):
+        s1 = "abc123"
+        s2 = "abc1000"
+        assert(numeric_string_cmp(s1, s2) < 0)
+
+    def test_numeric_string_cmp_2(self):
+        s1 = "Category 9"
+        s2 = "Category 11"
+        assert(numeric_string_cmp(s1, s2) < 0)
+
+    def test_numeric_string_cmp_3(self):
+        s1 = "3 blind mice"
+        s2 = "3 blind pigs"
+        assert(numeric_string_cmp(s1, s2) < 0)
+
+    def test_numeric_string_cmp_3(self):
+        s1 = "3 blind mice"
+        s2 = "13 blind mice"
+        assert(numeric_string_cmp(s1, s2) < 0)
+
+    def test_numeric_string_cmp_4(self):
+        s1 = "a"
+        s2 = "b"
+        assert(numeric_string_cmp(s1, s2) < 0)
