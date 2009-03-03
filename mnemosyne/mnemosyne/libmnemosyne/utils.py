ec9cb0f1ae8e57302e273e56661c124f704011da
Michael Appleby <ma@marvin.local>
2009-03-02 20:49:57 -0800

    Filled out the docstring comment for numeric_string_cmp()

--- mnemosyne/mnemosyne/libmnemosyne/utils.py
+++ mnemosyne/mnemosyne/libmnemosyne/utils.py
@@ -54,7 +54,22 @@ def contract_path(p, prefix=None):
 
 
 def numeric_string_cmp(s1, s2):
-    """Compare two strings using numeric ordering"""
+    
+    """Compare two strings using numeric ordering
+    
+    Compare the two strings s1 and s2 and return an integer according to the 
+    outcome. The return value is negative if s1 < s2, zero if s1 == s2 and 
+    strictly positive if s1 > s2. Unlike the standard python cmp() function
+    numeric_string_cmp() compares strings using a natural numeric ordering,
+    so that, e.g., "abc2" < "abc10".
+
+    The strings are first split into tuples consisting of the alphabetic and
+    numeric portions of the string. For example, "33_file1.txt" is converted
+    to the tuple ('', 33, '_file', 1, '.txt'). The tuples are then compared
+    using the standard python cmp().
+
+    """
+    
     atoi = lambda s: int(s) if s.isdigit() else s
-    scan = lambda s: tuple(atoi(c) for c in re.split('(\d+)', s))
+    scan = lambda s: tuple(atoi(str) for str in re.split('(\d+)', s))
     return cmp(scan(s1), scan(s2))
