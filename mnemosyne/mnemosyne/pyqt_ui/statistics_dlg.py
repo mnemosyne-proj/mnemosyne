b39c88022751f8be587f5495eb73e744b4debe3f
Michael Appleby <ma@marvin.local>
2009-03-04 20:33:39 -0800

    Added text statistics for schedule_info tab

--- mnemosyne/mnemosyne/pyqt_ui/statistics_dlg.py
+++ mnemosyne/mnemosyne/pyqt_ui/statistics_dlg.py
@@ -5,10 +5,10 @@
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 
+from mnemosyne.libmnemosyne.component_manager import database
 from ui_statistics_dlg import Ui_StatisticsDlg
 
-
-#class ListItem(QListViewItem):
+#class ListItem(QListViewItem):#{{{
 #    def __init__(self, parent, name, number):
 #        QListViewItem.__init__(self, parent, name, str(number))
 #        self.setMultiLinesEnabled(1)
@@ -18,7 +18,7 @@ from ui_statistics_dlg import Ui_StatisticsDlg
 #            return self.key(0,ascending).compare(item.key(0,ascending))
 #        else: # Funny casts for Windows compatibility.
 #            return int(str(self.text(1))) - int(str(item.text(1)))
-
+#}}}
 
 class StatisticsDlg(QDialog, Ui_StatisticsDlg):
 
@@ -26,20 +26,20 @@ class StatisticsDlg(QDialog, Ui_StatisticsDlg):
         QDialog.__init__(self,parent)
         self.setupUi(self)
 
-#        # Schedule information.
+        # Schedule information.
 
-#        text = self.trUtf8("Scheduled cards for next week:\n\n")
+        text = self.trUtf8("Scheduled cards for next week:\n\n")
 
-#        old_cumulative = 0
-#        for days in range(0,8):
-#            cumulative = scheduled_items(days)
-#            text.append(self.trUtf8("In")).\
-#                 append(QString(" " + str(days) + " ")).\
-#                 append(self.trUtf8("day(s) :")).\
-#                 append(QString(" " + str(cumulative-old_cumulative) + "\n"))
-#            old_cumulative = cumulative
+        old_cumulative = 0
+        for days in range(0,8):
+            cumulative = database().scheduled_count(days)
+            text.append(self.trUtf8("In")).\
+                 append(QString(" " + str(days) + " ")).\
+                 append(self.trUtf8("day(s) :")).\
+                 append(QString(" " + str(cumulative-old_cumulative) + "\n"))
+            old_cumulative = cumulative
 
-#        self.schedule_info.setText(text)
+        self.schedule_info.setText(text)
 
 #        # Grade information.
 
