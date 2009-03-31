89cb5711ae6c43a1de5dfd126fdad5cf8dfce0d4
Michael Appleby <ma@marvin.local>
2009-03-30 22:03:35 -0700

    Fixed a minor bug where functions were referencing a non-existent attribute database().days_since_start instead of calling database().days_since_start()

--- mnemosyne/mnemosyne/libmnemosyne/card.py
+++ mnemosyne/mnemosyne/libmnemosyne/card.py
@@ -79,9 +79,9 @@ class Card(object):
     interval = property(lambda self : self.next_rep - self.last_rep)
     
     days_since_last_rep = property(lambda self : \
-                            database().days_since_start - self.last_rep)
+                            database().days_since_start() - self.last_rep)
                             
     days_until_next_rep = property(lambda self : \
-                            self.next_rep - database().days_since_start)
+                            self.next_rep - database().days_since_start())
 
 
