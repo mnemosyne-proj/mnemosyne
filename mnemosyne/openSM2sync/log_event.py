d466a60eeeffb2359b1d53eb7fff890ed9152289
Peter Bienstman <Peter.Bienstman@UGent.be>
2009-11-12 13:32:32 +0100

    Add missing file.

---
+++ mnemosyne/openSM2sync/log_event.py
@@ -0,0 +1,33 @@
+#
+# log_event.py <Peter.Bienstman@UGent.be>
+#
+
+class EventCodes(object):
+
+    """Codes to identify event types."""
+
+    STARTED_PROGRAM = 1
+    STOPPED_PROGRAM = 2
+    STARTED_SCHEDULER = 3
+    LOADED_DATABASE = 4
+    SAVED_DATABASE = 5
+    ADDED_TAG = 6
+    UPDATED_TAG = 7
+    DELETED_TAG = 8
+    ADDED_FACT = 9
+    UPDATED_FACT = 10
+    DELETED_FACT = 11
+    ADDED_CARD = 12
+    UPDATED_CARD = 13
+    DELETED_CARD = 14
+    ADDED_CARD_TYPE = 15
+    UPDATED_CARD_TYPE = 16
+    DELETED_CARD_TYPE = 17
+    REPETITION = 18
+    ADDED_MEDIA = 19
+    DELETED_MEDIA = 20
+
+
+class LogEvent(object):
+
+    pass
