bac51aa48eed231844d8c72e5a60219648f97979
Peter Bienstman <Peter.Bienstman@UGent.be>
2011-08-31 21:43:25 +0200

    Add plugin to hide toolbar.

---
+++ mnemosyne/mnemosyne/example_plugins/hide_toolbar.py
@@ -0,0 +1,31 @@
+#
+# hide_toolbar.py <Peter.Bienstman@UGent.be>
+#
+
+from PyQt4 import QtCore, QtGui
+
+from mnemosyne.libmnemosyne.plugin import Plugin
+
+
+class HideToolbarPlugin(Plugin):
+    
+    name = "Hide toolbar"
+    description = "Hide the main toolbar"
+
+    def __init__(self, component_manager):
+        Plugin.__init__(self, component_manager)
+
+    def activate(self):
+        Plugin.activate(self)
+        self.main_widget().tool_bar.setVisible(False)
+
+    def deactivate(self):
+        Plugin.deactivate(self)
+        self.main_widget().tool_bar.setVisible(True)
+  
+
+# Register plugin.
+
+from mnemosyne.libmnemosyne.plugin import register_user_plugin
+register_user_plugin(HideToolbarPlugin)
+
