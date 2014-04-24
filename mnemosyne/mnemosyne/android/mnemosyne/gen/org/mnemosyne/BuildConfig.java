4c16730b79078132bb9ef221db6ee9ad5e54fb9a
Peter.Bienstman@UGent.be <>
2014-04-24 20:48:32 +0200

    Android review widget.

---
+++ mnemosyne/mnemosyne/android/mnemosyne/gen/org/mnemosyne/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package org.mnemosyne;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ mnemosyne/mnemosyne/android/mnemosyne/src/org/mnemosyne/ReviewActivity.java
@@ -0,0 +1,14 @@
+package org.mnemosyne;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ReviewActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+	
+}
