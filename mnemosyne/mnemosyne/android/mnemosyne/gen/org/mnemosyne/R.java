97af54d6c5509b313b4b0d66ea75f76f426ba0c5
Peter.Bienstman@UGent.be <>
2014-10-17 20:53:09 +0200

    Start sync ui.

--- mnemosyne/mnemosyne/android/mnemosyne/gen/org/mnemosyne/R.java
+++ mnemosyne/mnemosyne/android/mnemosyne/gen/org/mnemosyne/R.java
@@ -22,19 +22,22 @@ public final class R {
         public static final int button3=0x7f050008;
         public static final int button4=0x7f050009;
         public static final int button5=0x7f05000a;
-        public static final int editText1=0x7f05000e;
-        public static final int editText2=0x7f050010;
-        public static final int editText3=0x7f050012;
-        public static final int editText4=0x7f050014;
+        public static final int editDatabase=0x7f05000e;
+        public static final int editText1=0x7f050010;
+        public static final int editText2=0x7f050012;
+        public static final int editText3=0x7f050014;
+        public static final int editText4=0x7f050016;
         public static final int menu_sync=0x7f05000c;
         public static final int question=0x7f050001;
         public static final int questionLabel=0x7f050000;
         public static final int showAnswerButton=0x7f050004;
         public static final int statusbar=0x7f05000b;
-        public static final int textView1=0x7f05000d;
-        public static final int textView2=0x7f05000f;
-        public static final int textView3=0x7f050011;
-        public static final int textView4=0x7f050013;
+        public static final int syncButton=0x7f050017;
+        public static final int textView1=0x7f05000f;
+        public static final int textView2=0x7f050011;
+        public static final int textView3=0x7f050013;
+        public static final int textView4=0x7f050015;
+        public static final int textView5=0x7f05000d;
     }
     public static final class layout {
         public static final int main=0x7f030000;
--- mnemosyne/mnemosyne/android/mnemosyne/src/org/mnemosyne/SyncActivity.java
+++ mnemosyne/mnemosyne/android/mnemosyne/src/org/mnemosyne/SyncActivity.java
@@ -1,6 +1,11 @@
 package org.mnemosyne;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 
 
 public class SyncActivity extends Activity {
@@ -9,7 +14,22 @@ public class SyncActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.main_view);
-        //rest of the code
+        setContentView(R.layout.sync);
+
+        final EditText editDatabase =(EditText) findViewById(R.id.editDatabase);
+
+        Button button = (Button) findViewById(R.id.syncButton);
+
+        button.setOnClickListener(new OnClickListener() {
+
+            public void onClick(View arg0) {
+                String database = editDatabase.getText().toString();
+                Intent intent= new Intent();
+                intent.putExtra("DATABASE", database);
+
+                setResult(0, intent);
+                finish();
+            }
+        });
     }
 }
