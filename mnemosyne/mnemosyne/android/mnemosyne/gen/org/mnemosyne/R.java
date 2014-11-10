91c83a46132de386429acfc1fa0d565ccec5a78d
Peter.Bienstman@UGent.be <>
2014-11-10 21:37:39 +0100

    Start on 'activate cards'

--- mnemosyne/mnemosyne/android/mnemosyne/gen/org/mnemosyne/R.java
+++ mnemosyne/mnemosyne/android/mnemosyne/gen/org/mnemosyne/R.java
@@ -14,35 +14,37 @@ public final class R {
         public static final int mnemosyne=0x7f020000;
     }
     public static final class id {
-        public static final int answer=0x7f050003;
-        public static final int answerLabel=0x7f050002;
-        public static final int button0=0x7f050005;
-        public static final int button1=0x7f050006;
-        public static final int button2=0x7f050007;
-        public static final int button3=0x7f050008;
-        public static final int button4=0x7f050009;
-        public static final int button5=0x7f05000a;
-        public static final int editPassword=0x7f050016;
-        public static final int editPort=0x7f050012;
-        public static final int editServer=0x7f050010;
-        public static final int editUsername=0x7f050014;
-        public static final int menu_replay_media=0x7f05000d;
-        public static final int menu_star=0x7f05000e;
-        public static final int menu_sync=0x7f05000c;
-        public static final int question=0x7f050001;
-        public static final int questionLabel=0x7f050000;
-        public static final int showAnswerButton=0x7f050004;
-        public static final int statusbar=0x7f05000b;
-        public static final int syncButton=0x7f050017;
-        public static final int textView1=0x7f05000f;
-        public static final int textView2=0x7f050011;
-        public static final int textView3=0x7f050013;
-        public static final int textView4=0x7f050015;
+        public static final int answer=0x7f050004;
+        public static final int answerLabel=0x7f050003;
+        public static final int button0=0x7f050006;
+        public static final int button1=0x7f050007;
+        public static final int button2=0x7f050008;
+        public static final int button3=0x7f050009;
+        public static final int button4=0x7f05000a;
+        public static final int button5=0x7f05000b;
+        public static final int editPassword=0x7f050018;
+        public static final int editPort=0x7f050014;
+        public static final int editServer=0x7f050012;
+        public static final int editUsername=0x7f050016;
+        public static final int menu_activate=0x7f050010;
+        public static final int menu_replay_media=0x7f05000e;
+        public static final int menu_star=0x7f05000f;
+        public static final int menu_sync=0x7f05000d;
+        public static final int question=0x7f050002;
+        public static final int questionLabel=0x7f050001;
+        public static final int showAnswerButton=0x7f050005;
+        public static final int statusbar=0x7f05000c;
+        public static final int syncButton=0x7f050000;
+        public static final int textView1=0x7f050011;
+        public static final int textView2=0x7f050013;
+        public static final int textView3=0x7f050015;
+        public static final int textView4=0x7f050017;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
-        public static final int menu=0x7f030001;
-        public static final int sync=0x7f030002;
+        public static final int activate=0x7f030000;
+        public static final int main=0x7f030001;
+        public static final int menu=0x7f030002;
+        public static final int sync=0x7f030003;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
