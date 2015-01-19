2689350f8ef56f9605cc7f9f9026f22722a6c169
Peter.Bienstman@UGent.be <>
2015-01-19 21:15:54 +0100

    Add testcases for merging code.

---
+++ mnemosyne/tests/test_db_import.py
@@ -0,0 +1,73 @@
+#
+# test_tsv_import.py <Peter.Bienstman@UGent.be>
+#
+
+import os
+import shutil
+from nose.tools import raises
+
+from mnemosyne_test import MnemosyneTest
+from mnemosyne.libmnemosyne import Mnemosyne
+from openSM2sync.log_entry import EventTypes
+from mnemosyne.libmnemosyne.ui_components.dialogs import ImportDialog
+from mnemosyne.libmnemosyne.ui_components.main_widget import MainWidget
+
+last_error = ""
+
+class Widget(MainWidget):
+
+    def activate(self):
+        self.review_controller().reset()
+
+    def show_error(self, message):
+        global last_error
+        print message
+        last_error = message
+
+class TestDBImport(MnemosyneTest):
+
+    def setup(self):
+        self.initialise_data_dir()
+        self.mnemosyne = Mnemosyne(upload_science_logs=False, interested_in_old_reps=True,
+                    asynchronous_database=True)
+        self.mnemosyne.components.insert(0,
+           ("mnemosyne.libmnemosyne.translators.gettext_translator", "GetTextTranslator"))
+        self.mnemosyne.components.append(\
+            ("mnemosyne_test", "TestReviewWidget"))
+        self.mnemosyne.components.append(\
+            ("test_db_import", "Widget"))
+        self.mnemosyne.initialise(os.path.abspath("dot_test"), automatic_upgrades=False)
+        self.review_controller().reset()
+
+    def db_importer(self):
+        for format in self.mnemosyne.component_manager.all("file_format"):
+            if format.__class__.__name__ == "Mnemosyne2Db":
+                return format
+
+    def test_1(self):        
+        filename = os.path.join(os.getcwd(), "tests", "files", "config.db")
+        self.db_importer().do_import(filename)
+        assert "configuration database" in last_error
+        
+    def test_2(self):
+        fact_data = {"f": "question",
+                     "b": "answer"}
+        card_type = self.card_type_with_id("1")
+        old_card = self.controller().create_new_cards(fact_data, card_type,
+                                 grade=-1, tag_names=["default"])[0]
+        assert len([self.database().cards()]) == 1
+        
+        filename = os.path.join(os.getcwd(), "tests", "files", "to_merge.db")
+        
+        global last_error
+        last_error = ""
+        self.db_importer().do_import(filename)
+        assert last_error == ""
+        db = self.database()
+        assert db.con.execute("select count() from log").fetchone()[0] == 72
+        self.review_controller().reset()
+        assert self.database().card_count() == 5
+        assert self.database().active_count() == 4
+
+
+        
