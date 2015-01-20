bcb5dce3f575b015158f2f309f9ef171de577f87
Peter.Bienstman@UGent.be <>
2015-01-20 21:53:35 +0100

    Merging now keeps status of deactivated card types.

--- mnemosyne/mnemosyne/libmnemosyne/file_formats/mnemosyne2_db.py
+++ mnemosyne/mnemosyne/libmnemosyne/file_formats/mnemosyne2_db.py
@@ -32,6 +32,8 @@ class Mnemosyne2Db(FileFormat):
         cards_format = Mnemosyne2Cards(self.component_manager)
         tmp_cards_filename = os.path.join(data_dir, "TMP.cards")
         cards_format.do_export(tmp_cards_filename, used_for_merging_dbs=True)
+        old_deactivated_card_type_fact_view_ids = \
+            db.current_criterion().deactivated_card_type_fact_view_ids
         # Import the *.cards file into the receiving database.
         db.load(receiving_database_filename)
         log_index_before_import = db.current_log_index()
@@ -39,4 +41,7 @@ class Mnemosyne2Db(FileFormat):
             tmp_cards_filename, extra_tag_names, show_metadata=False)
         db.merge_logs_from_other_database(filename, log_index_before_import)
         os.remove(tmp_cards_filename)
+        db.current_criterion().deactivated_card_type_fact_view_ids.update(\
+            old_deactivated_card_type_fact_view_ids)
+        db.set_current_criterion(db.current_criterion())      
         
