f53f1c380bcdb59b99ac060179d229789737a569
Peter.Bienstman@UGent.be <>
2015-02-20 20:21:21 +0100

    Support media filenames with #.

--- mnemosyne/mnemosyne/libmnemosyne/databases/SQLite.py
+++ mnemosyne/mnemosyne/libmnemosyne/databases/SQLite.py
@@ -349,6 +349,8 @@ class SQLite(Database, SQLiteSync, SQLiteMedia, SQLiteLogging,
         if self.is_loaded():
             self.unload()
         self._path = expand_path(path, self.config().data_dir)
+        if not os.path.exists(self._path):
+            return new(path)
         # Check database version.
         try:
             sql_res = self.con.execute("""select value from global_variables
