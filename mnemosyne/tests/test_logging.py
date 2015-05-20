d2996e10d0587106a33715f62e5e64b05b548d1f
Peter.Bienstman@UGent.be <>
2015-05-20 21:29:42 +0200

    Update testsuite.

--- mnemosyne/tests/test_logging.py
+++ mnemosyne/tests/test_logging.py
@@ -3,6 +3,7 @@
 #
 
 import os
+import shutil
 
 from mnemosyne_test import MnemosyneTest
 from mnemosyne.libmnemosyne import Mnemosyne
@@ -360,3 +361,28 @@ class TestLogging(MnemosyneTest):
             ("mnemosyne_test", "TestReviewWidget"))
         self.mnemosyne.initialise(os.path.abspath("dot_test"), automatic_upgrades=False)
         self.mnemosyne.start_review()
+        
+    def mem_importer(self):
+        for format in self.mnemosyne.component_manager.all("file_format"):
+            if format.__class__.__name__ == "Mnemosyne1Mem":
+                return format        
+
+    def test_archive_old_logs(self):
+        # Import old history.
+        filename = os.path.join(os.getcwd(), "tests", "files", "basedir_bz2",
+                                "default.mem")
+        self.mem_importer().do_import(filename)        
+        assert self.database().con.execute("select count() from log").fetchone()[0] == 23
+        assert not os.path.exists(os.path.join("dot_test", "archive"))
+        # Archive.
+        self.database().archive_old_logs()
+        assert self.database().con.execute("select count() from log").fetchone()[0] == 12
+        archive_name = os.listdir(os.path.join(os.getcwd(), "dot_test", "archive"))[0]
+        archive_path = os.path.join(os.getcwd(), "dot_test", "archive", archive_name)
+        import sqlite3
+        arch_con = sqlite3.connect(archive_path)
+        assert arch_con.execute("select count() from log").fetchone()[0] == 11
+        
+
+        
+        
