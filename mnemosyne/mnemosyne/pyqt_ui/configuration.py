9b939cee75cb33a4a4eaf57c890e19d7d53084b4
Peter.Bienstman@UGent.be <>
2015-08-12 20:25:38 +0200

    Fixes for deactivate cards dialog.

--- mnemosyne/mnemosyne/pyqt_ui/configuration.py
+++ mnemosyne/mnemosyne/pyqt_ui/configuration.py
@@ -49,7 +49,6 @@ class PyQtConfiguration(Hook):
              "started_activate_cards_n_times": 0,
              "showed_help_on_renaming_sets": False,
              "showed_help_on_double_clicking_sets": False,
-             "showed_help_on_modifying_sets": False,
              "showed_help_on_adding_tags": False
             }.items():
             self.config().setdefault(key, value)
