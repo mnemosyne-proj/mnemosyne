131b19ced839eb23735f7f6e4d7533c7f95a6853
Peter.Bienstman@UGent.be <>
2016-09-26 18:51:15 +0200

    Better packaging Android.

--- mnemosyne/mnemosyne/android/app/src/main/java/org/mnemosyne/MnemosyneInstaller.java
+++ mnemosyne/mnemosyne/android/app/src/main/java/org/mnemosyne/MnemosyneInstaller.java
@@ -1,19 +1,21 @@
 package org.mnemosyne;
 
-import com.srplab.www.starcore.StarCoreFactoryPath;
-
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.app.ProgressDialog;
 import android.os.AsyncTask;
 import android.os.Handler;
 import android.util.Log;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.FileReader;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.BufferedWriter;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
-import java.util.ArrayList;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
@@ -22,30 +24,49 @@ public class MnemosyneInstaller extends AsyncTask<Void, Void, Void>  {
     private MnemosyneActivity UIActivity;
     private String basedir;
     Handler UIHandler;
+    private static String mAssetModifiedPath = "lastmodified.txt";
 
     public MnemosyneInstaller(MnemosyneActivity activity, Handler handler)
     {
         UIActivity = activity;
         UIHandler = handler;
-        basedir = "/data/data/" + UIActivity.getPackageName();
+
+        basedir = UIActivity.getApplicationInfo().dataDir;
     }
 
-    private void mergeApkFile(ArrayList<String> partFileList, String dst)
-            throws IOException {
-        if (!new File(partFileList.get(0)).exists()) {
-            OutputStream out = UIActivity.openFileOutput(dst, UIActivity.MODE_WORLD_READABLE);
-            byte[] buffer = new byte[1024];
-            InputStream in;
-            int readLen = 0;
-            for (int i=0; i<partFileList.size(); i++){
-                in = UIActivity.getAssets().open(partFileList.get(i));
-                while ((readLen = in.read(buffer)) != -1) {
-                    out.write(buffer, 0, readLen);
-                }
-                out.flush();
-                in.close();
-            }
-            out.close();
+    private void setAssetLastModified(long time) {
+        String filename = this.basedir + "/files/" + mAssetModifiedPath;
+        try {
+            BufferedWriter bwriter = new BufferedWriter(new FileWriter(new File(filename)));
+            bwriter.write(String.format("%d", time));
+            bwriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private long getAssetLastModified() {
+        String filename = this.basedir + "/files/" + mAssetModifiedPath;
+        try {
+            BufferedReader breader = new BufferedReader(new FileReader(new File(filename)));
+            String contents = breader.readLine();
+            breader.close();
+            return Long.valueOf(contents).longValue();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return 0;
+        }
+    }
+
+    private long getAppLastUpdate() {
+        PackageManager pm = UIActivity.getPackageManager();
+        try {
+            PackageInfo pkgInfo = pm.getPackageInfo(UIActivity.getPackageName(),
+                    PackageManager.GET_PERMISSIONS);
+            return pkgInfo.lastUpdateTime;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return 1;
         }
     }
 
@@ -132,127 +153,110 @@ public class MnemosyneInstaller extends AsyncTask<Void, Void, Void>  {
         }
     }
 
+    void deleteRecursive(File fileOrDirectory) {
+        if (fileOrDirectory.isDirectory())
+            for (File child : fileOrDirectory.listFiles())
+                deleteRecursive(child);
+
+        fileOrDirectory.delete();
+    }
+
     private ProgressDialog progressDialog;
     @Override
     protected void onPreExecute() {
-        progressDialog = new ProgressDialog(UIActivity);
-
-        progressDialog.setCancelable(false);
-        progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
-        progressDialog.setMessage("Preparing to run Mnemosyne...");
-        progressDialog.setIndeterminate(true);
-        progressDialog.show();
+        long appLastUpdate = this.getAppLastUpdate();
+        long assetLastModified = this.getAssetLastModified();
+        if (appLastUpdate > assetLastModified) {
+            progressDialog = new ProgressDialog(UIActivity);
+            progressDialog.setCancelable(false);
+            progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+            progressDialog.setMessage("Finalising Mnemosyne install...");
+            progressDialog.setIndeterminate(true);
+            progressDialog.show();
+        }
     }
 
     @Override
     protected Void doInBackground (Void... params) {
-        Log.d("Mnemosyne", "About to extract Mnemosyne");
-
-        File destDir = new File(basedir + "/files");
-
-        // TODO: do this on first starting new version
-        //deleteRecursive(destDir);
-
-        if( !destDir.exists() )
+        long appLastUpdate = this.getAppLastUpdate();
+        long assetLastModified = this.getAssetLastModified();
+        if (appLastUpdate <= assetLastModified) {
+            Log.i("Mnemosyne", "Assets are up to date");
+        } else {
+            Log.i("Mnemosyne", "Removing previous assets");
+            File destDir = new File(basedir + "/files");
+            deleteRecursive(destDir);
             destDir.mkdirs();
+            Log.d("Mnemosyne", "About to extract Mnemosyne");
+            try {
+                InputStream dataSource = UIActivity.getAssets().open("python3.4.zip");
+                unzip(dataSource, basedir + "/files", true);
 
-        //try {
-        //    InputStream dataSource = UIActivity.getAssets().open("python3.4.zip");
-        //    unzip(dataSource, basedir + "/files", true);
-        //}
-        //catch (IOException e) {
-        //    e.printStackTrace();
-        //}
-
-        //java.io.File pythonlibFile = new java.io.File(basedir +"/files/python3.4.zip");
-        //if( !pythonlibFile.exists() ){
-         //   try{
-         //       copyFile("python3.4.zip", "");
-         //   }
-        //    catch(Exception e){
-        //        e.printStackTrace();
-        //    }
-        //
-        // }
-        java.io.File zlibFile = new java.io.File(basedir + "/files/zlib.cpython-34m.so");
-        if( !zlibFile.exists() ){
-            try{
+                java.io.File zlibFile = new java.io.File(basedir + "/files/zlib.cpython-34m.so");
                 copyFile("zlib.cpython-34m.so", "");
-            }
-            catch(Exception e){
+
+                destDir = new File(basedir + "/files/lib-dynload");
+                destDir.mkdirs();
+
+                copyFile("_bisect.cpython-34m.so", "lib-dynload/");
+                copyFile("_codecs_cn.cpython-34m.so", "lib-dynload/");
+                copyFile("_codecs_hk.cpython-34m.so", "lib-dynload/");
+                copyFile("_codecs_iso2022.cpython-34m.so", "lib-dynload/");
+                copyFile("_codecs_jp.cpython-34m.so", "lib-dynload/");
+                copyFile("_codecs_kr.cpython-34m.so", "lib-dynload/");
+                copyFile("_codecs_tw.cpython-34m.so", "lib-dynload/");
+                copyFile("_crypt.cpython-34m.so", "lib-dynload/");
+                copyFile("_csv.cpython-34m.so", "lib-dynload/");
+                copyFile("_ctypes.cpython-34m.so", "lib-dynload/");
+                copyFile("_ctypes_test.cpython-34m.so", "lib-dynload/");
+                copyFile("_datetime.cpython-34m.so", "lib-dynload/");
+                copyFile("_elementtree.cpython-34m.so", "lib-dynload/");
+                copyFile("_heapq.cpython-34m.so", "lib-dynload/");
+                copyFile("_json.cpython-34m.so", "lib-dynload/");
+                copyFile("_lsprof.cpython-34m.so", "lib-dynload/");
+                copyFile("_md5.cpython-34m.so", "lib-dynload/");
+                copyFile("_multibytecodec.cpython-34m.so", "lib-dynload/");
+                copyFile("_multiprocessing.cpython-34m.so", "lib-dynload/");
+                copyFile("_opcode.cpython-34m.so", "lib-dynload/");
+                copyFile("_pickle.cpython-34m.so", "lib-dynload/");
+                copyFile("_posixsubprocess.cpython-34m.so", "lib-dynload/");
+                copyFile("_random.cpython-34m.so", "lib-dynload/");
+                copyFile("_sha1.cpython-34m.so", "lib-dynload/");
+                copyFile("_sha256.cpython-34m.so", "lib-dynload/");
+                copyFile("_sha512.cpython-34m.so", "lib-dynload/");
+                copyFile("_socket.cpython-34m.so", "lib-dynload/");
+                copyFile("_sqlite3.cpython-34m.so", "lib-dynload/");
+                copyFile("_struct.cpython-34m.so", "lib-dynload/");
+                copyFile("_testbuffer.cpython-34m.so", "lib-dynload/");
+                copyFile("_testcapi.cpython-34m.so", "lib-dynload/");
+                copyFile("_testimportmultiple.cpython-34m.so", "lib-dynload/");
+                copyFile("array.cpython-34m.so", "lib-dynload/");
+                copyFile("audioop.cpython-34m.so", "lib-dynload/");
+                copyFile("binascii.cpython-34m.so", "lib-dynload/");
+                copyFile("cmath.cpython-34m.so", "lib-dynload/");
+                copyFile("fcntl.cpython-34m.so", "lib-dynload/");
+                copyFile("grp.cpython-34m.so", "lib-dynload/");
+                copyFile("math.cpython-34m.so", "lib-dynload/");
+                copyFile("mmap.cpython-34m.so", "lib-dynload/");
+                copyFile("parser.cpython-34m.so", "lib-dynload/");
+                copyFile("pyexpat.cpython-34m.so", "lib-dynload/");
+                copyFile("resource.cpython-34m.so", "lib-dynload/");
+                copyFile("select.cpython-34m.so", "lib-dynload/");
+                copyFile("syslog.cpython-34m.so", "lib-dynload/");
+                copyFile("termios.cpython-34m.so", "lib-dynload/");
+                copyFile("time.cpython-34m.so", "lib-dynload/");
+                copyFile("unicodedata.cpython-34m.so", "lib-dynload/");
+                copyFile("xxlimited.cpython-34m.so", "lib-dynload/");
+                copyFile("zlib.cpython-34m.so", "lib-dynload/");
+
+                dataSource = UIActivity.getAssets().open("mnemosyne.zip");
+                unzip(dataSource, basedir + "/files", true);
+            } catch (Exception e) {
                 e.printStackTrace();
             }
+            this.setAssetLastModified(appLastUpdate);
+            Log.i("Mnemosyne", "Done extracting Mnemosyne");
         }
-
-        destDir = new File(basedir + "/files/lib-dynload");
-        if (!destDir.exists())
-            destDir.mkdirs();
-        try {
-            copyFile("_bisect.cpython-34m.so", "lib-dynload/");
-            copyFile("_codecs_cn.cpython-34m.so", "lib-dynload/");
-            copyFile("_codecs_hk.cpython-34m.so", "lib-dynload/");
-            copyFile("_codecs_iso2022.cpython-34m.so", "lib-dynload/");
-            copyFile("_codecs_jp.cpython-34m.so", "lib-dynload/");
-            copyFile("_codecs_kr.cpython-34m.so", "lib-dynload/");
-            copyFile("_codecs_tw.cpython-34m.so", "lib-dynload/");
-            copyFile("_crypt.cpython-34m.so", "lib-dynload/");
-            copyFile("_csv.cpython-34m.so", "lib-dynload/");
-            copyFile("_ctypes.cpython-34m.so", "lib-dynload/");
-            copyFile("_ctypes_test.cpython-34m.so", "lib-dynload/");
-            copyFile("_datetime.cpython-34m.so", "lib-dynload/");
-            copyFile("_elementtree.cpython-34m.so", "lib-dynload/");
-            copyFile("_heapq.cpython-34m.so", "lib-dynload/");
-            copyFile("_json.cpython-34m.so", "lib-dynload/");
-            copyFile("_lsprof.cpython-34m.so", "lib-dynload/");
-            copyFile("_md5.cpython-34m.so", "lib-dynload/");
-            copyFile("_multibytecodec.cpython-34m.so", "lib-dynload/");
-            copyFile("_multiprocessing.cpython-34m.so", "lib-dynload/");
-            copyFile("_opcode.cpython-34m.so", "lib-dynload/");
-            copyFile("_pickle.cpython-34m.so", "lib-dynload/");
-            copyFile("_posixsubprocess.cpython-34m.so", "lib-dynload/");
-            copyFile("_random.cpython-34m.so", "lib-dynload/");
-            copyFile("_sha1.cpython-34m.so", "lib-dynload/");
-            copyFile("_sha256.cpython-34m.so", "lib-dynload/");
-            copyFile("_sha512.cpython-34m.so", "lib-dynload/");
-            copyFile("_socket.cpython-34m.so", "lib-dynload/");
-            copyFile("_sqlite3.cpython-34m.so", "lib-dynload/");
-            copyFile("_struct.cpython-34m.so", "lib-dynload/");
-            copyFile("_testbuffer.cpython-34m.so", "lib-dynload/");
-            copyFile("_testcapi.cpython-34m.so", "lib-dynload/");
-            copyFile("_testimportmultiple.cpython-34m.so", "lib-dynload/");
-            copyFile("array.cpython-34m.so", "lib-dynload/");
-            copyFile("audioop.cpython-34m.so", "lib-dynload/");
-            copyFile("binascii.cpython-34m.so", "lib-dynload/");
-            copyFile("cmath.cpython-34m.so", "lib-dynload/");
-            copyFile("fcntl.cpython-34m.so", "lib-dynload/");
-            copyFile("grp.cpython-34m.so", "lib-dynload/");
-            copyFile("math.cpython-34m.so", "lib-dynload/");
-            copyFile("mmap.cpython-34m.so", "lib-dynload/");
-            copyFile("parser.cpython-34m.so", "lib-dynload/");
-            copyFile("pyexpat.cpython-34m.so", "lib-dynload/");
-            copyFile("resource.cpython-34m.so", "lib-dynload/");
-            copyFile("select.cpython-34m.so", "lib-dynload/");
-            copyFile("syslog.cpython-34m.so", "lib-dynload/");
-            copyFile("termios.cpython-34m.so", "lib-dynload/");
-            copyFile("time.cpython-34m.so", "lib-dynload/");
-            copyFile("unicodedata.cpython-34m.so", "lib-dynload/");
-            copyFile("xxlimited.cpython-34m.so", "lib-dynload/");
-            copyFile("zlib.cpython-34m.so", "lib-dynload/");
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        try {
-            InputStream dataSource = UIActivity.getAssets().open("mnemosyne.zip");
-            StarCoreFactoryPath.CreatePath(Runtime.getRuntime(), basedir + "/files");
-            unzip(dataSource, basedir + "/files", true);
-        }
-        catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        Log.d("Mnemosyne", "Extracted Mnemosyne");
 
         //String path = basedir + "/files/lib-dynload";
         //Log.d("Mnemosyne", "Path: " + path);
@@ -264,22 +268,14 @@ public class MnemosyneInstaller extends AsyncTask<Void, Void, Void>  {
         //    Log.d("Mnemosyne", "FileName:" + file[i].getName());
         //}
 
-
         return null;
-    }
-
-    void deleteRecursive(File fileOrDirectory) {
-        if (fileOrDirectory.isDirectory())
-            for (File child : fileOrDirectory.listFiles())
-                deleteRecursive(child);
-
-        fileOrDirectory.delete();
     }
 
     @Override
     protected void onPostExecute(Void result) {
-        progressDialog.dismiss();
+        if (progressDialog != null) {
+            progressDialog.dismiss();
+        }
         UIActivity.continueOnCreate();
     }
-
 }
