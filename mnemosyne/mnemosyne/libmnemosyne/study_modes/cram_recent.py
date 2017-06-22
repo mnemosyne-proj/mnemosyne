9c14ccdc10b6808f69e62bf2ecdf9d488b95008d
Peter Bienstman <Peter.Bienstman@UGent.be>
2017-06-22 09:13:35 +0200

    Added missing file.

---
+++ mnemosyne/mnemosyne/libmnemosyne/study_modes/cram_recent.py
@@ -0,0 +1,22 @@
+#
+# cram_recent.py <Peter.Bienstman@UGent.be>
+#
+
+from mnemosyne.libmnemosyne.translator import _
+from mnemosyne.libmnemosyne.study_mode import StudyMode
+from mnemosyne.libmnemosyne.schedulers.cramming import Cramming
+from mnemosyne.libmnemosyne.review_controllers.SM2_controller_cramming \
+     import SM2ControllerCramming
+
+
+class CramRecent(StudyMode):
+    
+    id = "CramRecent"
+    name = _("Cram recently learned new cards")
+    menu_weight = 4
+    Scheduler = Cramming
+    ReviewController = SM2ControllerCramming
+    
+    def activate(self):
+        self.activate_components()
+        self.review_controller().reset(new_only=True)    
