ff36061ed5a1542c2a7674c8726b9a7965027068
Marcin Mielniczuk <marmistrz.dev@zoho.eu>
2018-10-08 18:56:08 +0200

    Require the CRYSTAX_NDK to be set instead of hardcoding a Windows path.
    cc #84

--- mnemosyne/android/app/src/main/jni/Android.mk
+++ mnemosyne/android/app/src/main/jni/Android.mk
@@ -1,5 +1,8 @@
 LOCAL_PATH := $(call my-dir)
-CRYSTAX_PATH := C:/crystax-ndk-10.3.2
+
+ifndef CRYSTAX_NDK
+$(error Variable CRYSTAX_NDK unset)
+endif
 
 # Build libpybridge.so
 
@@ -15,8 +18,8 @@ include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE    := python3.5m
-LOCAL_SRC_FILES := $(CRYSTAX_PATH)/sources/python/3.5/libs/$(TARGET_ARCH_ABI)/libpython3.5m.so
-LOCAL_EXPORT_CFLAGS := -I $(CRYSTAX_PATH)/sources/python/3.5/include/python/
+LOCAL_SRC_FILES := $(CRYSTAX_NDK)/sources/python/3.5/libs/$(TARGET_ARCH_ABI)/libpython3.5m.so
+LOCAL_EXPORT_CFLAGS := -I $(CRYSTAX_NDK)/sources/python/3.5/include/python/
 include $(PREBUILT_SHARED_LIBRARY)
 
 # Python modules dependencies like _sqlite3.so have to be manually copied to
@@ -27,4 +30,3 @@ include $(PREBUILT_SHARED_LIBRARY)
 #LOCAL_SRC_FILES := $(CRYSTAX_PATH)/sources/python/3.5/libs/$(TARGET_ARCH_ABI)/modules/_sqlite3.so
 #LOCAL_EXPORT_CFLAGS := -I $(CRYSTAX_PATH)/sources/python/3.5/include/python/
 #include $(PREBUILT_SHARED_LIBRARY)
-
