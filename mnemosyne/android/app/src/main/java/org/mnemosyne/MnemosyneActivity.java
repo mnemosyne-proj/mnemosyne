6739aebd207405986577679a8ea43a79d4e430d0
Peter Bienstman <Peter.Bienstman@UGent.be>
2019-04-25 10:38:24 +0200

    Fix for escaped hex unicode in text file import.

--- mnemosyne/android/app/src/main/java/org/mnemosyne/MnemosyneActivity.java
+++ mnemosyne/android/app/src/main/java/org/mnemosyne/MnemosyneActivity.java
@@ -252,23 +252,20 @@ public class MnemosyneActivity extends AppCompatActivity {
                             //Toast toast = Toast.makeText(getApplicationContext(),
                             //        s, Toast.LENGTH_SHORT);
                             //toast.show();
-                            //Log.d("Mnemosyne", "visibility: " + Integer.toString(visibility) + " " +Integer.toString(View.SYSTEM_UI_FLAG_FULLSCREEN) );
+                            //Log.d("Mnemosyne", "visibility: " + Integer.toString(visibility));
                             if ((visibility & View.SYSTEM_UI_FLAG_FULLSCREEN) == 0) {
                                 // Exiting full screen, so show the action bar and hide
                                 // it 5 seconds later.
                                 getSupportActionBar().show();
-                                //Log.d("Mnemosyne", "show bar");
                                 new Handler().postDelayed(new Runnable() {
                                     @Override
                                     public void run() {
                                         setFullscreen();
-                                        //Log.d("Mnemosyne", "move to fullscreen");
                                     }
                                 }, 5000);
                             }
                             if (visibility == 0) {
                                 getSupportActionBar().show();
-                                //Log.d("Mnemosyne", "show bar");
                             }
                         }
                     });
--- mnemosyne/libmnemosyne/file_formats/tsv.py
+++ mnemosyne/libmnemosyne/file_formats/tsv.py
@@ -46,7 +46,13 @@ class Tsv(FileFormat, MediaPreprocessor):
             # Parse html style escaped unicode (e.g. &#33267;).
             for match in re0.finditer(line):
                 # Integer part.
-                u = chr(int(match.group(1)))
+                escaped = match.group(1)
+                if escaped[0] == "x":
+                    escaped = "0" + escaped
+                if "x" in escaped:
+                    u = chr(int(escaped, 16))
+                else:
+                    u = chr(int(escaped))
                 # Integer part with &# and ;.
                 line = line.replace(match.group(), u)
             if len(line) == 0:
