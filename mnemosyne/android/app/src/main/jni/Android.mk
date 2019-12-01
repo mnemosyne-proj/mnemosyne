fb87b7dc5bc0a5c34232cde551a8b72cfa7933aa
Peter Bienstman <Peter.Bienstman@UGent.be>
2019-12-01 09:47:12 +0100

    Android work.

--- mnemosyne/android/app/src/main/jni/Android.mk
+++ mnemosyne/android/app/src/main/jni/Android.mk
@@ -1,3 +1,5 @@
+# Migrate to: https://github.com/android/ndk-samples/tree/master/hello-libs
+
 # https://stackoverflow.com/questions/9870435/how-to-link-a-prebuilt-shared-library-to-an-android-ndk-project
 
 LOCAL_PATH := $(call my-dir)
@@ -5,15 +7,16 @@ LOCAL_PATH := $(call my-dir)
 # libpython3.7m
 include $(CLEAR_VARS)
 LOCAL_MODULE := python3.7m
-LOCAL_SRC_FILES := ../jniLibs/$(TARGET_ARCH_ABI)/libpython3.7m.so
-# TODO: better dir
-LOCAL_EXPORT_C_INCLUDES := C:/Users/peter/.conda/envs/testapp/android/$(TARGET_ARCH_ABI)/include/python3.7m
+LOCAL_SRC_FILES := $(LOCAL_PATH)/../conda-android-python/$(TARGET_ARCH_ABI)/lib/libpython3.7m.so
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/../conda-android-python/$(TARGET_ARCH_ABI)/include/python3.7m
 include $(PREBUILT_SHARED_LIBRARY)
 
 # libpybridge.so
 include $(CLEAR_VARS)
-LOCAL_MODULE    := pybridge
+LOCAL_MODULE := pybridge
 LOCAL_SRC_FILES := pybridge.c
-LOCAL_LDLIBS :=  -llog
+LOCAL_LDLIBS := -llog
+# Unsuccessful workaround: https://github.com/android/ndk-samples/issues/364
+LOCAL_CFLAGS += -L$(LOCAL_PATH)/../conda-android-python/$(TARGET_ARCH_ABI)/lib
 LOCAL_SHARED_LIBRARIES := python3.7m
 include $(BUILD_SHARED_LIBRARY)
