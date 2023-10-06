1aa2a7217fadfb9cd6a0cb0de41057a1315a21d8
Peter Bienstman <Peter.Bienstman@gmail.com>
2023-10-06 20:57:15 +0200

    Furigana fixes for Android.

--- mnemosyne/libmnemosyne/filters/furigana.py
+++ mnemosyne/libmnemosyne/filters/furigana.py
@@ -7,8 +7,8 @@ import re
 from mnemosyne.libmnemosyne.filter import Filter
 
 class Furigana(Filter):
-    
-    ruby_re = r"(\S+)\[([\u3040-\u309F]+)\]"
+
+    ruby_re = r"([\u4E00-\u9FFF]+)\[([\u3040-\u309F]+)\]"
 
     def run(self, text, card, fact_key, **render_args):
         return re.sub(self.ruby_re, r"<ruby>\1<rt>\2</rt></ruby>", text)
